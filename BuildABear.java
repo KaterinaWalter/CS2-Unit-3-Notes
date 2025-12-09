61db6c5f7fd387b336071aa7c09855e469f6c56a
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-12-09 16:08:28 +0000

    3.3 notes

---
+++ BuildABear.java
@@ -0,0 +1,22 @@
+/**
+ * BuildABear.java defines the BuildABear CLASS
+ * which is a template for BuildABear OBJECTS
+ */
+public class BuildABear {
+    // 3 SECTIONS IN AN OBJECT-CLASS!
+    // 1. INSTANCE VARIABLES (data/attributes/fields)
+    private String type; // just declare, don't assign
+    private String name; // every BuildABear has a name
+    private int fillLevel;
+    private double price;
+    private boolean hasSpeaker;
+    private String recording;
+    // ENCAPSULATION: keeping variables PRIVATE means
+    // they cannot be directly accessed outside of 
+    // this Java CLASS (data is protected from users)
+
+    // 2. CONSTRUCTORS (initalize values)
+
+    // 3. METHODS (behaviors/actions)
+
+}
