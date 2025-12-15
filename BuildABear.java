2eed82fd58852793ff58f7135de17af0f46c4313
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-12-15 14:41:31 +0000

    3.4 notes

--- BuildABear.java
+++ BuildABear.java
@@ -16,6 +16,26 @@ public class BuildABear {
     // this Java CLASS (data is protected from users)
 
     // 2. CONSTRUCTORS (initalize values)
+    // DEFAULT CONSTRUCTOR does not take any arguments (no-arg)
+    // what gets called when user says "= new BuildABear()"
+    public BuildABear() {
+        this.type = "bear"; // WE PICK the initial values!
+        this.name = "Teddy";
+        this.fillLevel = 75; 
+        this.price = 150.00;
+        this.hasSpeaker = false; 
+        this.recording = ""; 
+    }
+    // PARAMETERIZED CONSTRUCTOR allows user to provide args
+    // user PASSES initial values to the constructor call
+    public BuildABear(String t, String n, int fl) {
+        this.type = t; // USER picked the value for type!
+        this.name = n;
+        this.fillLevel = fl;
+        this.price = 150.00;
+        this.hasSpeaker = false;
+        this.recording = "";
+    }
 
     // 3. METHODS (behaviors/actions)
 
--- Main.java
+++ Main.java
@@ -10,7 +10,11 @@ public class Main {
 
       // All objects can be created using this pattern:
       // ClassName objectName = new ClassName();
-      BuildABear kitty = new BuildABear("cat");
+      BuildABear kitty = new BuildABear();
+      // "BuildABear kitty" is DECLARING a variable of type BuildABear
+      // "new BuildABear()" is CALLING a CONSTRUCTOR to set up the object
+
+      
 
    }
 }
